6a5bcb28985a5359652c86c3193f35179b128c3c
Albert <AlbertShym@users.noreply.github.com>
2016-11-22 11:15:33 -0800

    Add files via upload

---
+++ projectYahtzee/projectYahtzee/Yahtzee.vb
@@ -0,0 +1,18 @@
+﻿'=======================================================
+' Project:        Lab 2
+' Title:          projectYahtzee
+' File Name:      Yahtzee.vb
+' Date Completed: Not Available
+'
+' Authors:        Albert A. Shymanskyy - Programmer
+'                 Anthony T. Wallace - Programmer
+'                 Karolina A. Shymanska - Documentation
+'                 Alexander P. Easter - Designer
+' Class:          CS 115 Section B
+'
+' Description:    Not Available
+'=======================================================
+
+Public Class frmYahtzee
+
+End Class
